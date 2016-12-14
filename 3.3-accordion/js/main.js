77de20eca7ad2d38b9d8e95f69195a9f7e67b254
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2016-12-14 14:31:17 -0600

    cant seem to get my jquery commands to do anything

--- 3.3-accordion/js/main.js
+++ 3.3-accordion/js/main.js
@@ -1,16 +1,16 @@
 $(function(){
   // need to get data-js to main.js, im getting the first item on my list because that what I need to manipulate.
-  var $destinationsElement = $("[data-js='destinations']");
+  var $destinationElement = $("[data-js='destination']");
   // need a way to listen for an event and add a function call on that event
-  $destinationsElement.on("click", function(e){
+  $destinationElement.on("click", function(e){
     // declar a ver ref the function (e)
-    var $clickedDestination = $(this);
+    var $clickedElement = $(this);
     // when the element is clicked i need it to expand to show more information, but I also need the expanded destination to go back to its original place when another destination is "clicked".
-    $clickedDestination.filter(".more__info").removeClass("more__info");
+    $destinationElement.filter(".more__info").removeClass("more__info");
 
 
-    $clickedDestination.addClass("more__info");
-    console.log($);
+    $clickedElement.addClass("more__info");
+
   });
-
+console.log($);
 });
