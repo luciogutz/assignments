99e916f60300f6d1aab73511e6152ab4ef1ac89e
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2016-12-14 13:40:32 -0600

    added classes to HTML

--- 3.3-accordion/js/main.js
+++ 3.3-accordion/js/main.js
@@ -1,10 +1,13 @@
 $(function(){
+  // need to get data-js to main.js, im getting the first item on my list because that what I need to manipulate.
+  var $destinationsElement = $("[data-js='destination']");
+  // need a way to listen for an event and add a function call on that event
+  $destinationsElement.on("click", function(e){
+    // declar a ver ref the function (e)
+    var $clickedElement = $(this);
+    // when the element is clicked i need it to expand to show more information.
+    $clickedElement.expand()
 
-  var $locationsElement = $("[data-js='locations']");
-
-  
-
-
-
+  });
 
 });
