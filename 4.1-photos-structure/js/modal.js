2d92307fb53ec8cf7d8d0c0f358fb3beef9bdae8
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2016-12-20 22:24:49 -0600

    was able to get modal working on all buttons, will try to get imges to show as well

--- 4.1-photos-structure/js/modal.js
+++ 4.1-photos-structure/js/modal.js
@@ -2,14 +2,64 @@ var $ = require("jquery");
 
 $(function(){
   var $body = $("body");
-  var modal = $body.find("[data-js='modal1']");
-  var button1 = $body.find("[data-js='button1']");
+  var $modal = $body.find("[data-js='modal1']");
+  var $backButton = $body.find("[data-js='backButton']");
+  var $button1 = $body.find("[data-js='button1']");
+  var $button2 = $body.find("[data-js='button2']");
+  var $button3 = $body.find("[data-js='button3']");
+  var $button4 = $body.find("[data-js='button4']");
+  var $button5 = $body.find("[data-js='button5']");
+  var $button6 = $body.find("[data-js='button6']");
 
-  button1.on("click", function(e){
-    modal.removeClass("hidden");
-    modal.addClass("not__hidden");
+  // functions for album 1
+  $button1.on("click", function(e){
+    $modal.removeClass("hidden");
   });
-console.log(buttom1);
+  $backButton.on("click", function(e){
+    $modal.addClass("hidden");
+  });
+
+  // functions for album 2
+  $button2.on("click", function(e){
+    $modal.removeClass("hidden");
+  });
+  $backButton.on("click", function(e){
+    $modal.addClass("hidden");
+  });
+
+  // functions for album 3
+  $button3.on("click", function(e){
+    $modal.removeClass("hidden");
+  });
+  $backButton.on("click", function(e){
+    $modal.addClass("hidden");
+  });
+
+  // functions for album 4
+  $button4.on("click", function(e){
+    $modal.removeClass("hidden");
+  });
+  $backButton.on("click", function(e){
+    $modal.addClass("hidden");
+  });
+
+  // functions for album 5
+  $button5.on("click", function(e){
+    $modal.removeClass("hidden");
+  });
+  $backButton.on("click", function(e){
+    $modal.addClass("hidden");
+  });
+
+  // functions for album 6
+  $button6.on("click", function(e){
+    $modal.removeClass("hidden");
+  });
+  $backButton.on("click", function(e){
+    $modal.addClass("hidden");
+  });
+
+// console.log($button1);
 
 
 
