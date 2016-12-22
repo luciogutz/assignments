48237a5dc7811a228d8217485cf9051c3c79ec5a
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2016-12-21 21:27:17 -0600

    working on equals button

--- 4.3-refactor-calculator/dist/bundle.js
+++ 4.3-refactor-calculator/dist/bundle.js
@@ -200,15 +200,24 @@
 	var $ = __webpack_require__(2);
 
 	$(function(){
-	  var $this = $("this");
 	  var $body = $("body");
 	  var $outputElement = $body.find("[data-js='output']");
-	  var $exponentElement = $body.find("[data-js='exponent']");
+	  var $exponentElements = $body.find("[data-js='exponent']");
+	  var $equals = $body.find("[data-js='equal']");
 
-	  $calculatorElement.on("click", function(e){
-
+	  $exponentElements.on("click", function(e){
+	    var $clickedExponentElement = $(this);
+	    var currentOutputString = $outputElement.text();
+	    var numberClickedString = $clickedExponentElement.text();
+	    $outputElement.text(currentOutputString + numberClickedString);
 
 	  });
+	  $equals.on("click", function(e){
+	    var $equals = eval($outputElement);
+	    $outputElement.text($equals);
+	      console.log($equals);
+	  });
+
 	});
 
 
--- 4.3-refactor-calculator/js/refactor.js
+++ 4.3-refactor-calculator/js/refactor.js
@@ -1,13 +1,22 @@
 var $ = require("jquery");
 
 $(function(){
-  var $this = $("this");
   var $body = $("body");
   var $outputElement = $body.find("[data-js='output']");
-  var $exponentElement = $body.find("[data-js='exponent']");
+  var $exponentElements = $body.find("[data-js='exponent']");
+  var $equals = $body.find("[data-js='equal']");
 
-  $calculatorElement.on("click", function(e){
-
+  $exponentElements.on("click", function(e){
+    var $clickedExponentElement = $(this);
+    var currentOutputString = $outputElement.text();
+    var numberClickedString = $clickedExponentElement.text();
+    $outputElement.text(currentOutputString + numberClickedString);
 
   });
+  $equals.on("click", function(e){
+    var $equals = eval($outputElement);
+    $outputElement.text($equals);
+      console.log($equals);
+  });
+
 });
