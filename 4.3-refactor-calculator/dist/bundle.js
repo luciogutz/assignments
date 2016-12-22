de3ab4b2d4ade1885106a9a7bbef25450bf10efc
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2016-12-21 23:22:49 -0600

    was able to get my clear button working, and i found a problem with my outputElement. its not recognizing double digit numbers. dont why

--- 4.3-refactor-calculator/dist/bundle.js
+++ 4.3-refactor-calculator/dist/bundle.js
@@ -204,19 +204,24 @@
 	  var $outputElement = $body.find("[data-js='output']");
 	  var $exponentElements = $body.find("[data-js='exponent']");
 	  var $equals = $body.find("[data-js='equals']");
+	  var $clearButton = $body.find("[data-js='clear']");
 
 	  $exponentElements.on("click", function(e){
 	    var $clickedExponentElement = $(this);
 	    var currentOutputString = $outputElement.text();
 	    var numberClickedString = $clickedExponentElement.text();
-	    $outputElement.text(currentOutputString + numberClickedString);
-
+	    var totalNumber = currentOutputString + numberClickedString;
+	    $outputElement.text(totalNumber);
+	    console.log(currentOutputString);
 	  });
 	  $equals.on("click", function(e){
 	    var total = eval($outputElement.text());
-	        $outputElement.text(total);
+	    $outputElement.text(total);
 	  });
-
+	  $clearButton.on("click", function(){
+	    var $this = $("this");
+	    $this = $outputElement.text("");
+	  });
 
 	});
 
