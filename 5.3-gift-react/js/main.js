bc6d1eea68d316cc93f3f13b6b5292fff6827466
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-01-05 15:42:17 -0600

    added all my html elements in my main.js file. Im having trouble with getting it showing on the browser becasue im getting an error about ./main not found

--- 5.3-gift-react/js/main.js
+++ 5.3-gift-react/js/main.js
@@ -1,17 +1,26 @@
-import React from `react`
+import React from "react"
 
 export default React.createClass({
 
 
   render() {
     return (
-      <h1> Gifts to Buy </h1>
-      <main>
-        <form>
-        <input>
-        <button> ADD </button>
-        
+      <h1> Gifts To Buy </h1>
+      <main className="main">
+        <form className="giftItem__section">
+          <input className="giftItem" placeholder="type the gift to buy here">
+          <button className="giftSubmit" type="submit" value="post"> ADD </button>
+        </form>
+        <div className="overflow">
+          <section className="giftSelected">
+          </section>
+        </div>
       </main>
+        <div className="totalSection">
+          <p className="finalTotal"></p>
+          <p className="total"> Total: </p>
+        </div>
+
     )
 
 
