8108c5fb61aa632fe75f091b45c896462e5fe971
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-01-18 22:33:11 -0600

    finished all styling for adventurer mode but dont understand the underscore library

--- 7.2-contact/js/User.js
+++ 7.2-contact/js/User.js
@@ -6,13 +6,14 @@ export default React.createClass({
   render() {
     return (
       <section className="contact-Wrapper">
-        <img className="userImage" src="http://24x7webstudio.com/wp-content/uploads/2014/02/Boy-team.png"/>
-        <h2 className="userInfo"> {this.props.params.name} </h2>
-        <p className="userInfo"> {this.props.params.email} </p>
-        <p className="userInfo"> {this.props.params.phone} </p>
-        <p className="userInfo"> {this.props.params.location} </p>
-        <Link to='/'> Close </Link>
-
+        <Link className="backButton" to='/'> Back to contact list </Link>
+        <div className="userImage-Wrapper">
+          <img className="userImage" src="http://24x7webstudio.com/wp-content/uploads/2014/02/Boy-team.png"/>
+        </div>
+        <h2 className="userInfo"><img className="smallIcon" src="http://www.sunshineglobalhospitals.com/xadmin/myaccount/upload/default/profiledefault.png"/> {this.props.params.name} </h2>
+        <p className="userInfo"><img className="smallIcon2" src="http://downloadicons.net/sites/default/files/email-message-icon-82430.png"/> {this.props.params.email} </p>
+        <p className="userInfo"><img className="smallIcon2" src="http://image.flaticon.com/icons/png/512/0/191.png"/> {this.props.params.phone} </p>
+        <p className="userInfo"><img className="smallIcon2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/World_icon.svg/2000px-World_icon.svg.png"/> {this.props.params.location} </p>
       </section>
     )
   }
