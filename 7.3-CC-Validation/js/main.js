332af73db1b61c5e817011cdec4fa34070865452
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-01-19 22:01:55 -0600

    finished normal mode

--- 7.3-CC-Validation/js/main.js
+++ 7.3-CC-Validation/js/main.js
@@ -13,15 +13,19 @@ export default React.createClass({
       this.setState({isCardNumberValid:cardNumber})
   },
   onCardSubmit() {
-
+    if(this.state.isCardNumberValid === false){
+      this.refs.submit.pattern = ""
+    }else{
+      this.refs.submit.value = ""
+    }
 
   },
   render() {
     return(
       <section>
         <form>
-          <input className={this.state.isCardNumberValid ? "valid" : "invalid"} onChange={this.onInputChange} type="text"/>
-          <input className={this.state.isCardNumberValid ? "valid" : "invalid"} type="submit"/>
+          <input ref="submit" className={this.state.isCardNumberValid ? "valid" : "invalid"} onChange={this.onInputChange} type="text"/>
+          <input onClick={this.onCardSubmit} className={this.state.isCardNumberValid ? "valid" : "invalid"} type="submit"/>
         </form>
       </section>
     )
