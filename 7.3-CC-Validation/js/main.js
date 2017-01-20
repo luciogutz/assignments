a950351620e4776e18b9bcc0a734d9c467e1862d
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-01-19 22:36:14 -0600

    added a thank you after valid card has been validated and submitted

--- 7.3-CC-Validation/js/main.js
+++ 7.3-CC-Validation/js/main.js
@@ -9,22 +9,26 @@ export default React.createClass({
   },
   onInputChange(e) {
     var cardNumber = Validator.isCreditCard(e.target.value);
-    console.log(cardNumber);
       this.setState({isCardNumberValid:cardNumber})
+      this.refs.thankYou.className = "hidden"
   },
-  onCardSubmit() {
+  onCardSubmit(e) {
     if(this.state.isCardNumberValid === false){
       this.refs.submit.pattern = ""
     }else{
+      e.preventDefault()
       this.refs.submit.value = ""
+      this.refs.thankYou.className = "visible"
     }
   },
   render() {
     return(
       <section>
+        <h2> Enter Card Information Here </h2>
         <form>
           <input ref="submit" className={this.state.isCardNumberValid ? "valid" : "invalid"} onChange={this.onInputChange} type="text"/>
           <input onClick={this.onCardSubmit} className={this.state.isCardNumberValid ? "valid" : "invalid"} type="submit"/>
+          <h3 ref="thankYou" className="hidden"> Thank you, card accepted </h3>
         </form>
       </section>
     )
