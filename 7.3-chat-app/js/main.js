d34cf49286b0ea8129a8efaa1d2d47132e294060
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-01-22 23:47:26 -0600

    finished normal mode and styled it. was having trouble figuring out hoe to make the chat post section move up when a new chat message was put in.

--- 7.3-chat-app/js/main.js
+++ 7.3-chat-app/js/main.js
@@ -49,20 +49,50 @@ export default React.createClass({
   render() {
     return(
       <section>
-        <aside>
-          <h1> This is the side section </h1>
+        <aside className="aside">
+          <h2 className="tiy"> TIY San Antonio </h2>
+          <h4 className="channel"> Channels </h4>
+          <div className="channels">
+            <p className="channel-marg"> all-things-spurs</p>
+            <p className="channel-marg"> # announcments </p>
+            <p className="channel-marg"> # fee </p>
+            <p className="channel-marg"> # fee-lecture-notes </p>
+            <p className="channel-marg"> # js-junkie </p>
+            <p className="channel-marg"> # random </p>
+          </div>
+          <h4 className="channels"> Direct Message </h4>
+          <h6 className="user-marg"> daniel </h6>
+          <h6 className="user-marg"> david </h6>
+          <h6 className="user-marg"> terika </h6>
+          <h6 className="user-marg"> reese </h6>
+          <h6 className="user-marg"> nic </h6>
+          <h6 className="user-marg"> jason </h6>
+          <h6 className="user-marg"> jeremy </h6>
+          <h6 className="user-marg"> justin </h6>
+          <h6 className="user-marg"> jason </h6>
         </aside>
-        <article className="mainSection">
-          {
-            this.state.chatHistory.map((chat, i)=>{
-              return <p className="posts"><span key={i}> { chat.currentChat } </span></p>
-            })
-          }
-        <form>
-          <input onChange={this.onChatChange} placeholder="Type in message here" type="text"/>
-          <input onClick={ this.onChatSubmit } type="submit"/>
+        <section className="navBar">
+          <img className="navIcons" src="http://downloadicons.net/sites/default/files/christmas-gift-box-icon-64524.png"/>
+          <img className="navIcon-star" src="https://www.dukehealth.org/sites/default/themes/dukezen/images/icons/star-o-grey.svg"/>
+          <img className="navIcon-at" src="http://cliparts101.com/files/187/0C5623693068369CD580C6AFA8D51CE6/EmailIconWhiteonGrey.png"/>
+            <input placeholder="Search" className="searchBar" type="text"/>
+          <img className="navIcon-chat" src="http://www.iconsdb.com/icons/preview/gray/list-ingredients-xxl.png"/>
+          <img className="navIcon-settings" src="http://www.iconsdb.com/icons/download/icon-sets/grey-wall/settings-512.gif"/>
+          <img className="navIcon-phone" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdV5gPulD9DWrZcigUhvq_qETk1rg1eGifr4GT20w-CdL57gAz"/>
+        </section>
+        <section>
+          <article className="mainSection">
+              {
+                this.state.chatHistory.map((chat, i)=>{
+                  return <p className="posts"><img className="chatImage" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGHyL5TZXzyU0eXFXmNeKzxVVt9CxUxYRN77aOfhl4KgjqVujW-w"/><h4 className="userName">User Name: </h4><span key={i}> { chat.currentChat } </span></p>
+                })
+              }
+          </article>
+        <form className="chatInput">
+          <button className="submitButton" onClick={ this.onChatSubmit } type="submit"> + </button>
+          <input className="chatBar" onChange={this.onChatChange} placeholder="Type message here" type="text"/>
         </form>
-        </article>
+      </section>
       </section>
     )
   },
